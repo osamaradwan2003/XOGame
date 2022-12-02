22fb3fad24b2719b3f1a67698f504a1338d2450e
Osama Amin <osamaradwan2003@gmail.com>
2022-12-02 07:09:58 +0200

    ceheck Winner Function

--- src/logic/XOPlayerService.ts
+++ src/logic/XOPlayerService.ts
@@ -1,22 +1,52 @@
-import { Player } from "../pages";
-
 class XOPlayerService {
-  private state: string[][];
   private players: Player[];
   constructor(state: string[][], players: Player[]) {
-    this.state = state;
     this.players = players;
   }
 
-  checkWins(): Player {
+  checkWins(state: string[][]): Player {
     // check rows
-    this.state.map((row) => {
+    state.map((row) => {
       if (row.every((cell) => cell == this.players[0].name)) {
         return this.players[0];
+      } else if (row.every((cell) => cell == this.players[1].name)) {
+        return this.players[1];
       }
     });
 
-    return { name: "null" };
+    // check cols
+    for (let index = 0; index < 3; index++) {
+      let columns = state.map((cell) => cell[index]);
+      if (columns.every((cell) => cell == this.players[0].name)) {
+        return this.players[0];
+      } else if (columns.every((cell) => cell == this.players[1].name)) {
+        return this.players[1];
+      }
+    }
+
+    // check digonal
+    const diagonal1 = [state[0][0], state[1][1], state[2][2]];
+    const diagonal2 = [state[0][2], state[1][1], state[2][0]];
+
+    if (
+      diagonal1.every((cell) => cell == this.players[0].name) ||
+      diagonal2.every((cell) => cell == this.players[0].name)
+    ) {
+      return this.players[0];
+    }
+
+    if (
+      diagonal1.every((cell) => cell == this.players[1].name) ||
+      diagonal2.every((cell) => cell == this.players[1].name)
+    ) {
+      return this.players[1];
+    }
+
+    if (state.flat().every((cell) => cell != "")) {
+      return { name: "draw", isCPU: false, isPlayer: false, isFrist: false };
+    }
+
+    return { name: "null", isCPU: false, isPlayer: false, isFrist: false };
   }
 }
 
